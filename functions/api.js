b504ac64395fab5d5fff3f8a3f340a69467c3868
Marius Filip <mariusnicolae.filip@endava.com>
2024-09-11 13:32:24 +0300

    second commit

--- functions/api.js
+++ functions/api.js
@@ -2,10 +2,33 @@ const express = require("express");
 const serverless = require("serverless-http");
 const app = express();
 const router = express.Router();
+const axios = require("axios");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use("/.netlify/functions/api", router);
 
 router.get("/", (req, res) => {
-  res.json({ "App is running on port...": port });
+  res.send("something");
 });
 
-app.use("/.netlify/functions/api", router);
+const { parsed: config } = dotenv.config();
+
+const BASE_URL = `https://api.cloudinary.com/v1_1/${config.CLOUD_NAME}/resources/search?max_results=9999`;
+
+const auth = {
+  username: config.API_KEY,
+  password: config.API_SECRET,
+};
+
+router.get("/photos", async (req, res) => {
+  const response = await axios.get(BASE_URL, {
+    auth,
+  });
+  return res.send(response.data);
+});
+
 module.exports.handler = serverless(app);
